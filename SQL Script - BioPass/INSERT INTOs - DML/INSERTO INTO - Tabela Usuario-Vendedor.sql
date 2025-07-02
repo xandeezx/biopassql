b5041e3408c5e07c415bb7b6368f1a07ced4d4ec
Daniel Gonçalves <83424429+dev-danielg@users.noreply.github.com>
2025-07-02 13:34:03 -0300

    Update INSERTO INTO - Tabela Usuario-Vendedor.sql

--- SQL Script - BioPass/INSERT INTOs - DML/INSERTO INTO - Tabela Usuario-Vendedor.sql
+++ SQL Script - BioPass/INSERT INTOs - DML/INSERTO INTO - Tabela Usuario-Vendedor.sql
@@ -1,100 +1,99 @@
 insert into usuario values
 (default, 'José Felipe', 'jose391', 'josefe@gmail.com', '57636475057');
 insert into vendedor values
-(last_insert_id(), '30346665000112', 'Empresa');
+(last_insert_id(), '10000000000101', 'Empresa');
 
 insert into usuario values
 (default, 'Mario Augusto', 'mario@43', 'marioaug@hotmail.com', '9629878238');
 insert into vendedor values
-(last_insert_id(), '29431148048', 'Empreendedor');
+(last_insert_id(), '10000000101', 'Empreendedor');
 
 insert into usuario values
 (default, 'Ana Beatriz', 'anaB2025', 'anabeatriz@gmail.com', '21987650001');
 insert into vendedor values
-(last_insert_id(), '11222333000181', 'Empresa');
+(last_insert_id(), '10000000000102', 'Empresa');
 
 insert into usuario values
 (default, 'Lucas Pereira', 'lucasP!45', 'lucaspereira@yahoo.com', '21987650002');
 insert into vendedor values
-(last_insert_id(), '23456789012', 'Empreendedor');
+(last_insert_id(), '10000000102', 'Empreendedor');
 
 insert into usuario values
 (default, 'Mariana Rocha', 'mariRocha12', 'marianarocha@hotmail.com', '21987650003');
 insert into vendedor values
-(last_insert_id(), '33444555000183', 'Empresa');
+(last_insert_id(), '10000000000103', 'Empresa');
 
 insert into usuario values
 (default, 'Felipe Santos', 'feliPe#34', 'felipesantos@gmail.com', '21987650004');
 insert into vendedor values
-(last_insert_id(), '45678901234', 'Empreendedor');
+(last_insert_id(), '10000000103', 'Empreendedor');
 
 insert into usuario values
 (default, 'Carla Fernandes', 'carlaF_67', 'carla.fernandes@outlook.com', '21987650005');
 insert into vendedor values
-(last_insert_id(), '55666777000185', 'Empresa');
+(last_insert_id(), '10000000000104', 'Empresa');
 
 insert into usuario values
 (default, 'Rafael Oliveira', 'rafaO2025', 'rafaeloliveira@yahoo.com', '21987650006');
 insert into vendedor values
-(last_insert_id(), '67890123456', 'Empreendedor');
+(last_insert_id(), '10000000104', 'Empreendedor');
 
 insert into usuario values
 (default, 'Patrícia Lima', 'patriL!90', 'patricialima@gmail.com', '21987650007');
 insert into vendedor values
-(last_insert_id(), '77888999000187', 'Empresa');
+(last_insert_id(), '10000000000105', 'Empresa');
 
 insert into usuario values
 (default, 'Gustavo Almeida', 'gustA1234', 'gustavo.almeida@hotmail.com', '21987650008');
 insert into vendedor values
-(last_insert_id(), '89012345678', 'Empreendedor');
+(last_insert_id(), '10000000105', 'Empreendedor');
 
 insert into usuario values
 (default, 'Bianca Costa', 'bianC_56', 'biancacosta@gmail.com', '21987650009');
 insert into vendedor values
-(last_insert_id(), '99000111000189', 'Empresa');
+(last_insert_id(), '10000000000106', 'Empresa');
 
 insert into usuario values
 (default, 'Eduardo Nunes', 'eduN*78', 'eduardonunes@yahoo.com', '21987650010');
 insert into vendedor values
-(last_insert_id(), '90123456789', 'Empreendedor');
+(last_insert_id(), '10000000106', 'Empreendedor');
 
 insert into usuario values
 (default, 'Juliana Gomes', 'juliG2025', 'julianagomes@hotmail.com', '21987650011');
 insert into vendedor values
-(last_insert_id(), '12123233000191', 'Empresa');
+(last_insert_id(), '10000000000107', 'Empresa');
 
 insert into usuario values
 (default, 'Rodrigo Alves', 'rodrigA34', 'rodrigoalves@gmail.com', '21987650012');
 insert into vendedor values
-(last_insert_id(), '23456789019', 'Empreendedor');
+(last_insert_id(), '10000000107', 'Empreendedor');
 
 insert into usuario values
 (default, 'Tatiana Martins', 'tatiM!12', 'tatianamartins@yahoo.com', '21987650013');
 insert into vendedor values
-(last_insert_id(), '34455555000193', 'Empresa');
+(last_insert_id(), '10000000000108', 'Empresa');
 
 insert into usuario values
 (default, 'Bruno Silva', 'bruS_56', 'brunosilva@hotmail.com', '21987650014');
 insert into vendedor values
-(last_insert_id(), '45678901237', 'Empreendedor');
+(last_insert_id(), '10000000108', 'Empreendedor');
 
 insert into usuario values
 (default, 'Larissa Rocha', 'lariR2025', 'larissarocha@gmail.com', '21987650015');
 insert into vendedor values
-(last_insert_id(), '56667777000195', 'Empresa');
+(last_insert_id(), '10000000000109', 'Empresa');
 
 insert into usuario values
 (default, 'Marcos Pereira', 'marP#78', 'marcospereira@yahoo.com', '21987650016');
 insert into vendedor values
-(last_insert_id(), '67890123456', 'Empreendedor');
+(last_insert_id(), '10000000109', 'Empreendedor');
 
 insert into usuario values
 (default, 'Renata Souza', 'renaS_90', 'renatasouza@hotmail.com', '21987650017');
 insert into vendedor values
-(last_insert_id(), '78889999000197', 'Empresa');
+(last_insert_id(), '10000000000110', 'Empresa');
 
 insert into usuario values
 (default, 'Felipe Costa', 'felico2025', 'felipecosta@gmail.com', '21987650018');
 insert into vendedor values
-(last_insert_id(), '89012345678', 'Empreendedor');
-
+(last_insert_id(), '10000000110', 'Empreendedor');
